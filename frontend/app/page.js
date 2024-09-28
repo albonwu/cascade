a4cddc311c9a863d9755d4c3999d1050ea8ffd8c
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-09-28 13:28:22 -0400

    create basic layout for game

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,23 +3,42 @@ import { useCallback, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const [text, setText] = useState(`
+  const [css, setCss] = useState(`
 div {
   height: 100px;
   width: 100px;
 }`);
-  const onChange = useCallback((val, viewUpdate) => {
-    console.log("val", val), setText(val);
+  const [html, setHtml] = useState("<div></div>");
+
+  const handleCssEditorChange = useCallback((val, viewUpdate) => {
+    setCss(val);
   });
+  const handleHtmlChange = useCallback(() => {});
   return (
-    <CodeMirror
-      value={text}
-      height="200px"
-      theme={tokyoNight}
-      extensions={[loadLanguage("css")]}
-      onChange={onChange}
-    />
+    <div className={styles.container}>
+      <div className={styles.previewContainer}>
+        <img src="https://placehold.co/400" alt="target" />
+        <img src="https://placehold.co/400" alt="preview" />
+      </div>
+      <div className={styles.editorContainer}>
+        <CodeMirror
+          value={html}
+          height="200px"
+          theme={tokyoNight}
+          extensions={[loadLanguage("html")]}
+          onChange={handleHtmlChange}
+        />
+        <CodeMirror
+          value={css}
+          height="200px"
+          theme={tokyoNight}
+          extensions={[loadLanguage("css")]}
+          onChange={handleCssEditorChange}
+        />
+      </div>
+    </div>
   );
 }
