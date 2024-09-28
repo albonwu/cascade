b989711616c4d5b1675239963440026fde0b6d4e
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-09-28 14:16:46 -0400

    add live preview

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,6 +4,7 @@ import CodeMirror from "@uiw/react-codemirror";
 import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
 import styles from "./page.module.css";
+import DOMPurify from "dompurify";
 
 export default function Home() {
   const [css, setCss] = useState(`
@@ -11,7 +12,11 @@ div {
   height: 100px;
   width: 100px;
 }`);
-  const [html, setHtml] = useState("<div></div>");
+  const [html, setHtml] = useState("<div>hello</div>");
+
+  function generatePreviewHtml() {
+    return `<html><style>${css}</style>${DOMPurify.sanitize(html)}</html>`;
+  }
 
   const handleCssEditorChange = useCallback((val, viewUpdate) => {
     setCss(val);
@@ -21,7 +26,10 @@ div {
     <div className={styles.container}>
       <div className={styles.previewContainer}>
         <img src="https://placehold.co/400" alt="target" />
-        <img src="https://placehold.co/400" alt="preview" />
+        <iframe
+          className={styles.preview}
+          srcdoc={generatePreviewHtml()}
+        ></iframe>
       </div>
       <div className={styles.editorContainer}>
         <CodeMirror
