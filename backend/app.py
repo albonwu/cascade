2bf8593768f9803fee2c628baefb5a8334b09008
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-09-28 23:24:36 -0400

    bump for heroku deploy

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ cors = CORS(app)
 
 @app.route("/", methods=["GET"])
 def index():
-    return "this is the cascade backend lesgoooooo"
+    return "this is the cascade backend lesgooooooo"
 
 
 @app.route("/submit", methods=["POST"])
