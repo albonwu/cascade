8f17894458b175a06833d7e821dac1d3f6a2a626
albonwu <albonwu@umich.edu>
2024-09-29 00:49:23 -0400

    redux

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,3 +1,4 @@
+import { StoreProvider } from '../store/storeProvider';
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
   );
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,8 +7,20 @@ import { EyeDropper } from "react-eyedrop";
 import styles from "./page.module.css";
 import DOMPurify from "dompurify";
 import Home from "../components/views/home";
-
+import { useState } from "react";
+import Start from "../components/views/start";
+import { useSelector } from "react-redux";
 
 export default function Root() {
-  return <Home />
+  const pageState = useSelector((state) => state.example.value);
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.page}>
+          {pageState === "start" && <Start />}
+          {pageState === "home" && <Home />}
+        </div>
+      </div>
+    </>
+  );
 }
