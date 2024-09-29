0df895756b4b0f76a13a1ad6339fe3ccf5986b3d
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-09-28 20:03:42 -0400

    create backend route to generate components

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
+import os
 import time
+import json
 from uuid import uuid4
 from flask import Flask, request
 import imgkit
+import requests
+from dotenv import load_dotenv
 from flask_cors import CORS, cross_origin
 
+load_dotenv()
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
@@ -29,3 +34,41 @@ def handle_submit():
         },
     )
     return "<p>Hello world!</p>"
+
+
+BREADBOARD_URL = "https://breadboard-community.wl.r.appspot.com/boards/@ArtisticJellyfish/cascade-generator.bgl.api/run"
+
+
+@app.route("/generate", methods=["GET"])
+def generate_component():
+    res = requests.post(
+        BREADBOARD_URL,
+        json={
+            "$key": os.getenv("BREADBOARD-KEY"),
+            "context": "Theme: Playful, Component: <button>",
+        },
+    )
+    # print(f"{res.text = }")
+    json_res = res.text[5:]
+    data = json.loads(json_res)
+
+    print(data)
+
+    raw_output = data[1]["outputs"]["context"][-1]["parts"][0]["text"]
+
+    raw_output_as_json = raw_output.strip("`json ")
+
+    code_json = json.loads(raw_output_as_json)
+
+    imgkit.from_string(
+        f"<style>{code_json['css']}</style>{code_json['html']}",
+        "output.png",
+        options={
+            "crop-h": "400",
+            "crop-w": "400",
+            "--height": "400",
+            "--width": "400",
+        },
+    )
+
+    return code_json["css"]
